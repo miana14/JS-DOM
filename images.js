2956bb30039f1a89ad86defa2127ebf6b9eb6f6e
ThierryBouedo <thierry.bouedo@free.fr>
2023-01-04 21:20:42 +0100

    first commit

---
+++ images.js
@@ -0,0 +1,10 @@
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+
+left.addEventListener('click', function() {
+    // CODE HERE
+});
+
+right.addEventListener('click', function() {
+    // CODE HERE
+});
